d23d6cc2429522baca141303fe8e41b65272690d
Gang Zhao <crumpx@gmail.com>
2016-10-11 00:34:48 -0400

    uploaded

---
+++ js/portfolio.js
@@ -0,0 +1,54 @@
+
+var slideIndex = 1;
+var x = document.getElementsByClassName("showitem");
+
+window.addEventListener('resize',resized);
+
+function showDivs(n) {
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} 
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
+
+function plusDivs(n) {
+ 		showDivs(slideIndex += n);
+}
+
+function resized (){
+	if ($(document).width() < 1400) {
+		$('.arrow-box').css('display','');
+		showDivs(slideIndex);
+	} else {
+		for (i = 0; i < x.length; i++) {
+        	x[i].style.display = ""; 
+    	}
+    	$('.arrow-box').css('display','none')
+	}
+}
+
+if ($(document).width() < 1400) {
+	$('.arrow-box').css('display','');
+	showDivs(slideIndex);
+}
+
+
+	$(".about-link").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".about").offset().top},
+        'slow');
+});
+
+$(".portfolio-link").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#portfolio").offset().top},
+        'slow');
+});
+
+$(".contact-link").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".contact").offset().top},
+        'slow');
+});
